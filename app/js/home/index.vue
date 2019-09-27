0551327340ab7bc1ebc1fa027f459b995c0b6b7b
xzw <460542390@qq.com>
2019-09-27 17:37:54 +0800

    header swiper

--- app/js/home/index.vue
+++ app/js/home/index.vue
@@ -1,18 +1,22 @@
 <template lang="html">
-    <div :class="$style.home">
-        <h1>Home</h1>
+    <div>
+       <Heador />
+       <Slider />
     </div>
 </template>
 
 <script>
-export default {
-}
+	import Heador from '../public/header.vue'
+	import Slider from './hsilder.vue'
+	export default {
+		components:{
+			Heador,
+			Slider
+		}
+	}
 </script>
 
 <style lang="scss" module>
-  @import '../../css/reset.scss';
-  .home{
-    color: red;
-    font-size: 80px;
-  }
+
+ 
 </style>
