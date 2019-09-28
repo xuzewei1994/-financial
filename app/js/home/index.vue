52e122469cbc2a3bbd99e1214a0d4908436fd05d
xzw <460542390@qq.com>
2019-09-28 10:27:47 +0800

    home complete

--- app/js/home/index.vue
+++ app/js/home/index.vue
@@ -2,16 +2,38 @@
     <div>
        <Heador />
        <Slider />
+       <Novice />
+       <Borrow />
+       <Money />
+       <Product />
+       <Life />
+       <Footor />
+       <Nvabar />
     </div>
 </template>
 
 <script>
 	import Heador from '../public/header.vue'
+	import Footor from '../public/footer.vue'
+	import Nvabar from '../public/navbar.vue'
 	import Slider from './hsilder.vue'
+	import Novice from './novice.vue'
+	import Borrow from './borrow.vue'
+	import Money from './money.vue'
+	import Product from './product.vue'
+	import Life from './life.vue'
+	
 	export default {
 		components:{
 			Heador,
-			Slider
+			Slider,
+			Novice,
+			Borrow,
+			Money,
+			Product,
+			Life,
+			Footor,
+			Nvabar
 		}
 	}
 </script>
